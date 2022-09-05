321d5a27d12b6d480e43c23369548e9e06f96376
Chris Luong <christopher_luong@live.ca>
2022-09-05 17:15:55 -0400

    add validation to add and update blog endpoint

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,4 +1,6 @@
 const Blog = require('../models/blog')
+const { body, validationResult } = require("express-validator")
+
 
 let getBlogs = async function(req,res,next){
     let blogs = await Blog.find({});
@@ -10,42 +12,86 @@ let getBlog = async function(req,res,next){
     res.status(200).send({data:{blog}})
 }
 
-let addBlog = function(req,res,next){
-    const blog = new Blog(
-        {
-            title: req.body.title,
-            text: req.body.text,
-            author: "author",
-            dateCreated: Date.now()
+let addBlog = [
+    body("title")
+    .trim()
+    .isLength({min:1})
+    .escape()
+    .withMessage("Title required"),
+    body("text")
+        .trim()
+        .isLength({min:1})
+        .escape()
+        .withMessage("Text required")
+    ,
+    function(req,res,next){
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            res.status(400).send({
+                message: req.body,
+                errors: errors.array()
+            })
         }
-    )
-    blog.save((err)=>{
-        if(err) {
-            return next(err)
-        }
-        res.send("success")
-    })
-    
-}
-
-let updateBlog = function(req,res,next){
-    const updatedBlog = new Blog(
-        {
-            title: req.body.title,
-            text: req.body.text,
-            dateUpdated: Date.now(),
-            _id: req.params.id,
+        const blog = new Blog(
+            {
+                title: req.body.title,
+                text: req.body.text,
+                author: "author",
+                dateCreated: Date.now()
+            }
+        )
+        blog.save((err)=>{
+            if(err) {
+                return next(err)
+            }
+            else {
+                res.send("success")
+            }
             
-        }
-    )
-    Blog.findByIdAndUpdate(req.params.id, updatedBlog, {}, (err)=>{
-        if(!err) {
-            return next(err)
-        }
-        res.status(200).send("updated post")
-    })
+        })
+        
+    }   
+]
+
+let updateBlog = [
+    body("title")
+    .trim()
+    .isLength({min:1})
+    .escape()
+    .withMessage("Title required"),
+    body("text")
+        .trim()
+        .isLength({min:1})
+        .escape()
+        .withMessage("Text required")
     
-}
+    ,
+    function(req,res,next){
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            res.status(400).send({
+                message: req.body,
+                errors: errors.array()
+            })
+        }
+        const updatedBlog = new Blog(
+            {
+                title: req.body.title,
+                text: req.body.text,
+                dateUpdated: Date.now(),
+                _id: req.params.id,
+                
+            }
+        )
+        Blog.findByIdAndUpdate(req.params.id, updatedBlog, {}, (err)=>{
+            if(!err) {
+                return next(err)
+            }
+            res.status(200).send("updated post")
+        })
+        
+    }
+]
 
 let deleteBlog = function(req,res,next){
     Blog.findOneAndDelete({_id:req.params.id}, function(err) {
