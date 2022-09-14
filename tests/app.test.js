9b9d6ad66543cf9ddb3f4d10d4a6e6dc1fc12316
Chris Luong <christopher_luong@live.ca>
2022-09-14 15:26:14 -0400

    add tests for blog endpoints

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,21 +1,17 @@
 const blogRouter = require('../routes/blog');
 const request = require("supertest");
 const createServer = require('../utils/server')
-// const {MongoClient} = require('mongodb');
-
 const {MongoMemoryServer} = require('mongodb-memory-server')
 
 const dotenv = require('dotenv');
 dotenv.config();
 
-
-
-
 describe('blog module', ()=>{
-  // let connection;
-  // let db;
   let app;
   let instance;
+  let blogId;
+  let title;
+  let text;
     
   beforeAll(async()=>{
     instance = await MongoMemoryServer.create();
@@ -33,7 +29,31 @@ describe('blog module', ()=>{
     //need to delete all the data after, maybe this should be afterEach?
     instance.stop();
   })
-  describe("get blog", ()=>{
+  describe("blog router", ()=>{
+    describe('given the blog does not exist',()=>{
+      it("should return a 200 status and no results when queried for by id", async ()=>{
+        let invalidId = "invalidid"
+        let {body, statusCode} = await request(app).get(`/blog/`)
+        .query({
+          _id: invalidId
+        })
+        expect(statusCode).toBe(200)  
+        expect(body.data.blogs.length).toBe(0)
+      })
+      it("should return a 200 status and the blog when created", async ()=>{
+        title = "sample title"
+        text = "sample text"
+        let {body,statusCode} = await request(app)
+          .post("/blog")
+          .send(
+            {
+              title,
+              text
+            }
+          )    
+          expect(statusCode).toBe(200)
+      })
+    })
     describe("given the blog does exist", ()=>{
       beforeAll(async ()=>{
         let {body,statusCode} = await request(app)
@@ -44,19 +64,21 @@ describe('blog module', ()=>{
               text: "sample text",
             }
           )    
-          console.log(body) 
+        blogId = body.data.blog._id
       })
       it("should return a 200 status", async ()=>{
         let {body, statusCode} = await request(app).get("/blog")
         expect(statusCode).toBe(200)  
-        expect(body.data.blogs.length).toBe(1)
+        expect(body.data.blogs.length).not.toBe(0)
+      })
+      it("should return a 200 status and the correct blog", async()=> {
+        let {body, statusCode} = await request(app).get(`/blog/${blogId}`)
+        expect(statusCode).toBe(200)  
+        expect(body.data.blog._id).toBe(blogId)
+        expect(body.data.blog.title).toBe(title)
+        expect(body.data.blog.text).toBe(text)
       })
     })
-  })
-  test("testing jest works", (done) => {
-    request(app)
-      .get("/blog/test/test/")
-      .expect(200, done)
   })
 });
 
